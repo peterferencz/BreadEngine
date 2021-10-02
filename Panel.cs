3fdb7dffba14d2f6a7d93e1c6158c51a617efed6
Chicken-Potato <ferenczp2005@gmail.com>
2021-10-02 10:44:01 +0200

    Finally writing out the names of the components; Now Supports text overflow to the next line

--- Panel.cs
+++ Panel.cs
@@ -22,35 +22,47 @@ namespace tui_generator
         bool firstDrawCall = true;
         public void Draw(){
             if(firstDrawCall){
+                //Fill the cellsverticalmatrix with empty arrays
                 for (int i = 0; i < /*Count of rows, need outsideinfo*/panelCount; i++) {
                     cellsVerticalMatrix.Add(new ArrayList());
                 }
+                //Add cells to cellsverticalmatrix based on their
+                //matrixY position
                 for (int i = 0; i < cells.Count; i++) {
                     Cell cell = (Cell)cells[i];
                     ((ArrayList)cellsVerticalMatrix[cell.matrixY]).Add(cell);
                 }
+                //remove the elements from the arraylist
+                //with the help of another arrylist
+                ArrayList newArray = new ArrayList();
                 for (int i = 0; i < cellsVerticalMatrix.Count; i++) {
-                    if (((ArrayList)cellsVerticalMatrix[i]).Count == 0) {
-                        cellsVerticalMatrix.Remove(cellsVerticalMatrix[i]);
+                    if (((ArrayList)cellsVerticalMatrix[i]).Count != 0) {
+                        newArray.Add(cellsVerticalMatrix[i]);
                     }
                 }
+                cellsVerticalMatrix = newArray;
+
                 firstDrawCall = false;
             }
 
             int ScreenWidth = Console.WindowWidth;
             int ScreenHeight = Console.WindowHeight;
 
+            int objectIndex = 0;
+            int textIndex = 0;
             for (int rowCount = 0; rowCount < cellsVerticalMatrix.Count; rowCount++) {
-                for (int cellIndex = 0; cellIndex < ((ArrayList)cellsVerticalMatrix[rowCount]).Count; cellIndex++) {
-                    Cell cell = (Cell)((ArrayList)cellsVerticalMatrix[rowCount])[cellIndex];
-                    bool l,r,t,b;
-                    l = isOurCell(cell.matrixX-1, cell.matrixY);
-                    r = isOurCell(cell.matrixX+1, cell.matrixY);
-                    t = isOurCell(cell.matrixX, cell.matrixY-1);
-                    b = isOurCell(cell.matrixX, cell.matrixY+1);
-                    for (int x = 0; x < cell.Width; x++) {
-                        for (int y = 0; y < cell.Height; y++) {
-                            char toWrite = ((string)objects[0])[0];
+                ArrayList currentRow = (ArrayList)cellsVerticalMatrix[rowCount];
+                Cell lastCell = (Cell)currentRow[currentRow.Count-1];
+                for (int y = 0; y < ((Cell)currentRow[0]).Height; y++) {
+                    for (int cellIndex = 0; cellIndex < currentRow.Count; cellIndex++) {
+                        Cell cell = (Cell)currentRow[cellIndex];
+                        bool l,r,t,b;
+                        l = isOurCell(cell.matrixX-1, cell.matrixY);
+                        r = isOurCell(cell.matrixX+1, cell.matrixY);
+                        t = isOurCell(cell.matrixX, cell.matrixY-1);
+                        b = isOurCell(cell.matrixX, cell.matrixY+1);
+                        for (int x = 0; x < cell.Width; x++) {
+                            char toWrite = '*';
 
                             if (!t && !l && x == 0 && y == 0) {
                                 toWrite = '┌';
@@ -66,8 +78,34 @@ namespace tui_generator
                                 toWrite = '─';
                             } else{
                                 Console.ForegroundColor = ConsoleColor.DarkGray;
-                                toWrite = (rowCount+x).ToString()[0];
+                                if(objectIndex >= objects.Count){
+                                    toWrite = ' ';
+                                }else{
+                                    string objString = (string)objects[objectIndex];
+                                    if(textIndex >= objString.Length){
+                                        //If textIndex out of range
+
+                                        if(cell.Equals(lastCell) && x == cell.Width-2){
+                                            //Last char of line
+                                            if (textIndex < objString.Length) {
+                                                //Haven't finished string
+                                            }else{
+                                                //Finished string
+                                                textIndex = 0;
+                                                objectIndex++;
+                                            }
+                                            toWrite = ' ';
+                                        }else{
+                                            toWrite = ' ';
+                                        }
+                                    }else{
+                                        //Textindex is not out of range
+                                        toWrite = objString[textIndex];
+                                        textIndex++;
+                                    }
+                                }
                             }
+
                             Console.SetCursorPosition((cell.Width * cell.matrixX) + x, (cell.Height * cell.matrixY) + y);
                             Console.Write(toWrite);
                             Console.ResetColor();
@@ -76,11 +114,44 @@ namespace tui_generator
                 }
             }
 
-            foreach (ArrayList row in cellsVerticalMatrix) {
-                foreach (Cell cell in row) {
+
+            // for (int rowCount = 0; rowCount < cellsVerticalMatrix.Count; rowCount++) {
+            //     for (int cellIndex = 0; cellIndex < ((ArrayList)cellsVerticalMatrix[rowCount]).Count; cellIndex++) {
                     
-                }
-            }
+                    
+            //         Cell cell = (Cell)((ArrayList)cellsVerticalMatrix[rowCount])[cellIndex];
+            //         bool l,r,t,b;
+            //         l = isOurCell(cell.matrixX-1, cell.matrixY);
+            //         r = isOurCell(cell.matrixX+1, cell.matrixY);
+            //         t = isOurCell(cell.matrixX, cell.matrixY-1);
+            //         b = isOurCell(cell.matrixX, cell.matrixY+1);
+            //         for (int x = 0; x < cell.Width; x++) {
+            //             for (int y = 0; y < cell.Height; y++) {
+            //                 char toWrite = ((string)objects[0])[0];
+
+            //                 if (!t && !l && x == 0 && y == 0) {
+            //                     toWrite = '┌';
+            //                 } else if(!t && !r && x == cell.Width-1 && y == 0) {
+            //                     toWrite = '┐';
+            //                 } else if(!l && !b && x == 0 && y == cell.Height-1) {
+            //                     toWrite = '└';
+            //                 } else if(!r && !b && x == cell.Width-1 && y == cell.Height-1) {
+            //                     toWrite = '┘';
+            //                 } else if((x == 0 && !l) || (x == cell.Width-1 && !r)) {
+            //                     toWrite = '│';
+            //                 } else if((y == 0 && !t) || (y == cell.Height-1 && !b)) {
+            //                     toWrite = '─';
+            //                 } else{
+            //                     Console.ForegroundColor = ConsoleColor.DarkGray;
+            //                     toWrite = (rowCount+x).ToString()[0];
+            //                 }
+            //                 Console.SetCursorPosition((cell.Width * cell.matrixX) + x, (cell.Height * cell.matrixY) + y);
+            //                 Console.Write(toWrite);
+            //                 Console.ResetColor();
+            //             }
+            //         }
+            //     }
+            // }
 
             // for (int i = 0; i < cells.Count; i++) {
             //     Cell cell = (Cell)cells[i];
@@ -129,13 +200,22 @@ namespace tui_generator
             return false;
         }
 
-        // [][][][]
-        // X [][][]
-        // X X X []
-        // [][][][]
+        // [0] [1] [2]  [3]         [0,1]           0,1
+        // [4] [5] [6]  [7]         [4,5]
+        // [8] [9][10] [11]         [8,9]
+        // [12][13][14][15]         [12,13]
     }
 
     public struct Cell{
         public int Width, Height, matrixX, matrixY;
+
+        public override bool Equals(object obj) {
+            if(obj.GetType() == this.GetType()){
+                Cell other = (Cell)obj;
+                return (matrixX == other.matrixX) && (matrixY == other.matrixY);
+            }else{
+                return false;
+            }
+        }
     }
 }
